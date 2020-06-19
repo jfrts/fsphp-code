6fbe2d82268f97d08b79d69da92def17c9321279
Júlio Freitas <jcsr.frts@gmail.com>
2020-06-19 12:20:58 -0300

    05.03-Erros,_conexão_e_execução

--- 05-banco-de-dados-com-pdo/05.03-erros-execucao-e-conexao/index.php
+++ 05-banco-de-dados-com-pdo/05.03-erros-execucao-e-conexao/index.php
@@ -7,6 +7,18 @@ fullStackPHPClassName("05.03 - Errors, conexão e execução");
  */
 fullStackPHPClassSession("controle de erros", __LINE__);
 
+try {
+
+//    throw new Exception("Exception");
+    throw new PDOException("Exception");
+
+} catch (PDOException | ErrorException $exception) {
+    var_dump($exception);
+} catch (Exception $exception) {
+    echo "<p class='trigger error'>{$exception->getMessage()}</p>";
+} finally {
+    echo "<p class='trigger'>Execução terminou!</p>";
+}
 
 /*
  * [ php data object ] Uma classe PDO para manipulação de banco de dados.
@@ -14,9 +26,43 @@ fullStackPHPClassSession("controle de erros", __LINE__);
  */
 fullStackPHPClassSession("php data object", __LINE__);
 
+try {
+
+    $pdo = new PDO(
+        "mysql:host=localhost;dbname=fsphp;port=3306",
+        "root",
+        "",
+        [
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        ]
+    );
+
+    $stmt = $pdo->query("SELECT * FROM users LIMIT 3");
+
+    while ($user = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        var_dump($user);
+    }
+
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
 
 /*
  * [ conexão com singleton ] Conextar e obter um objeto PDO garantindo instância única.
  * http://br.phptherightway.com/pages/Design-Patterns.html
  */
 fullStackPHPClassSession("conexão com singleton", __LINE__);
+
+require __DIR__ . "/../source/autoload.php";
+
+use Source\Database\Connect;
+
+$pdo1 = Connect::getInstance();
+$pdo2 = Connect::getInstance();
+
+var_dump(
+    $pdo1,
+    $pdo2,
+    Connect::getInstance()::getAvailableDrivers(),
+    Connect::getInstance()->getAttribute(PDO::ATTR_DRIVER_NAME)
+);

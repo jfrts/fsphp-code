3048defa83f17dcee2b09cccb1e1ffdf83007720
Júlio Freitas <jcsr.frts@gmail.com>
2020-06-20 13:56:53 -0300

    05.04-Consultas_com_query_e_exec

--- 05-banco-de-dados-com-pdo/05.04-consultas-com-query-e-exec/index.php
+++ 05-banco-de-dados-com-pdo/05.04-consultas-com-query-e-exec/index.php
@@ -15,6 +15,28 @@ use Source\Database\Connect;
  */
 fullStackPHPClassSession("insert", __LINE__);
 
+$insert = "
+    INSERT INTO users (first_name, last_name, email, document) 
+    VALUES ('Julio', 'Freitas', 'cursos@teste.com', '12345678900'); 
+";
+
+try {
+
+//    $exec = Connect::getInstance()->exec($insert);
+//    var_dump(Connect::getInstance()->lastInsertId());
+    $exec = null;
+
+    $query = Connect::getInstance()->query($insert);
+    var_dump(Connect::getInstance()->lastInsertId());
+
+    var_dump(
+        $exec,
+        $query->errorInfo()
+    );
+
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
 
 /*
  * [ select ] Ler/Consultar dados.
@@ -22,6 +44,19 @@ fullStackPHPClassSession("insert", __LINE__);
  */
 fullStackPHPClassSession("select", __LINE__);
 
+try {
+
+    $query = Connect::getInstance()->query("SELECT * FROM users");
+
+    var_dump(
+        $query,
+        $query->rowCount(),
+        $query->fetchAll()
+    );
+
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
 
 /*
  * [ update ] Atualizar dados.
@@ -29,9 +64,34 @@ fullStackPHPClassSession("select", __LINE__);
  */
 fullStackPHPClassSession("update", __LINE__);
 
+try {
+
+    $exec = Connect::getInstance()->exec(
+        "UPDATE users SET first_name = 'Teste'
+        WHERE id = '53'"
+    );
+
+    var_dump($exec);
+
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
 
 /*
  * [ delete ] Deletar dados.
  * https://mariadb.com/kb/en/library/delete/
  */
 fullStackPHPClassSession("delete", __LINE__);
+
+try {
+
+    $exec = Connect::getInstance()->exec("
+        DELETE FROM users
+        WHERE id > 50
+    ");
+
+    var_dump($exec);
+
+} catch (PDOException $exception) {
+    var_dump($exception);
+}

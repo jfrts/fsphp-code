3a719fdd42f51c416173d78b0801e43a2c5a7725
Júlio Freitas <jcsr.frts@gmail.com>
2020-06-22 10:25:53 -0300

    05.07-PDOStatement_e_bind_modes

--- 05-banco-de-dados-com-pdo/05.07-pdostatements-e-bind-modes/index.php
+++ 05-banco-de-dados-com-pdo/05.07-pdostatements-e-bind-modes/index.php
@@ -11,6 +11,18 @@ use Source\Database\Connect;
  */
 fullStackPHPClassSession("prepared statement", __LINE__);
 
+$stmt = Connect::getInstance()->prepare("
+    SELECT * FROM users LIMIT 1
+");
+
+$stmt->execute();
+
+var_dump(
+    $stmt,
+    $stmt->rowCount(),
+    $stmt->columnCount(),
+    $stmt->fetch()
+);
 
 /*
  * [ bind value ] http://php.net/manual/pt_BR/pdostatement.bindvalue.php
@@ -18,20 +30,100 @@ fullStackPHPClassSession("prepared statement", __LINE__);
  */
 fullStackPHPClassSession("stmt bind value", __LINE__);
 
+$stmt = Connect::getInstance()->prepare("
+    SELECT * FROM users WHERE id = :id
+");
+
+$stmt->bindValue(":id", 50, PDO::PARAM_INT);
+
+$stmt->execute();
+
+var_dump(
+    $stmt->fetch()
+);
+
+/**
+ *
+ */
+
+$stmt = Connect::getInstance()->prepare("
+    INSERT INTO users (first_name, last_name)
+    VALUES (:first_name, :last_name)
+");
+
+$stmt->bindValue(":first_name", "Júlio", PDO::PARAM_STR);
+$stmt->bindValue(":last_name", "Freitas", PDO::PARAM_STR);
+
+$stmt->execute();
+
+var_dump(
+    $stmt->rowCount(),
+    Connect::getInstance()->lastInsertId()
+);
 
 /*
  * [ bind param ] http://php.net/manual/pt_BR/pdostatement.bindparam.php
  */
 fullStackPHPClassSession("stmt bind param", __LINE__);
 
+$stmt = Connect::getInstance()->prepare("
+    INSERT INTO users (first_name, last_name)
+    VALUES (:first_name, :last_name)
+");
+
+$firstName = "Júlio";
+$lastName = "Freitas";
+
+$stmt->bindParam(":first_name", $firstName, PDO::PARAM_STR);
+$stmt->bindParam(":last_name", $lastName, PDO::PARAM_STR);
+
+$stmt->execute();
+
+var_dump(
+    $stmt->rowCount(),
+    Connect::getInstance()->lastInsertId()
+);
 
 /*
  * [ execute ] http://php.net/manual/pt_BR/pdostatement.execute.php
  */
 fullStackPHPClassSession("stmt execute array", __LINE__);
 
+$stmt = Connect::getInstance()->prepare("
+    INSERT INTO users (first_name, last_name)
+    VALUES (:first_name, :last_name)
+");
+
+$firstName = "Júlio";
+$lastName = "Freitas";
+
+$user = [
+  "first_name" => "Júlio",
+  "last_name" => "Freitaas"
+];
+
+$stmt->execute($user);
+
+var_dump(
+    $stmt->rowCount(),
+    Connect::getInstance()->lastInsertId()
+);
 
 /*
  * [ bind column ] http://php.net/manual/en/pdostatement.bindcolumn.php
  */
 fullStackPHPClassSession("bind column", __LINE__);
+
+$stmt = Connect::getInstance()->prepare("
+    SELECT * FROM users LIMIT 3
+");
+
+$stmt->execute();
+
+$stmt->bindColumn("first_name", $name);
+$stmt->bindColumn("email", $email);
+
+while ($user = $stmt->fetch()) {
+    var_dump($user);
+    echo "<p class='trigger'>O email de {$name} é {$email}</p>";
+}
